f17ab9c436146241de69a6c66c733787d9bee166
Exrienz <exrienz@gmail.com>
2025-02-13 11:49:42 +0800

    Update SecretFinder.py

--- BurpSuite-SecretFinder/SecretFinder.py
+++ BurpSuite-SecretFinder/SecretFinder.py
@@ -33,45 +33,50 @@ class BurpExtender(IBurpExtender, IScannerCheck):
         else:
             return 0
 
-    # add your regex here
-    regexs = {
-        'google_api' : 'AIza[0-9A-Za-z-_]{35}',
-        'docs_file_exetension' : '^.*\.(xls|xlsx|doc|docx)$',
-        'bitcoin_address' : '([13][a-km-zA-HJ-NP-Z0-9]{26,33})',
-        'slack_api_key' : 'xox.-[0-9]{12}-[0-9]{12}-[0-9a-zA-Z]{24}',
-        'us_cn_zipcode' : '/(^\d{5}(-\d{4})?$)|(^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$)/',
-        'google_cloud_platform_auth' : '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}',
-        'google_cloud_platform_api' : '[A-Za-z0-9_]{21}--[A-Za-z0-9_]{8}',
-        'amazon_secret_key' : '[0-9a-zA-Z/+]{40}',
-        'gmail_auth_token' : '[0-9(+-[0-9A-Za-z_]{32}.apps.qooqleusercontent.com',
-        'github_auth_token' : '[0-9a-fA-F]{40}',
-        'Instagram_token' : '[0-9a-fA-F]{7}.[0-9a-fA-F]{32}',
-        'twitter_access_token' : '[1-9][ 0-9]+-(0-9a-zA-Z]{40}',
-        'firebase' : 'AAAA[A-Za-z0-9_-]{7}:[A-Za-z0-9_-]{140}',
-        'google_captcha' : '6L[0-9A-Za-z-_]{38}|^6[0-9a-zA-Z_-]{39}$',
-        'google_oauth' : 'ya29\.[0-9A-Za-z\-_]+',
-        'amazon_aws_access_key_id' : 'A[SK]IA[0-9A-Z]{16}',
-        'amazon_mws_auth_toke' : 'amzn\\.mws\\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}',
-        'amazon_aws_url' : 's3\.amazonaws.com[/]+|[a-zA-Z0-9_-]*\.s3\.amazonaws.com',
-        'facebook_access_token' : 'EAACEdEose0cBA[0-9A-Za-z]+',
-        'authorization_basic' : 'basic\s*[a-zA-Z0-9=:_\+\/-]+',
-        'authorization_bearer' : 'bearer\s*[a-zA-Z0-9_\-\.=:_\+\/]+',
-        'authorization_api' : 'api[key|\s*]+[a-zA-Z0-9_\-]+',
-        'mailgun_api_key' : 'key-[0-9a-zA-Z]{32}',
-        'twilio_api_key' : 'SK[0-9a-fA-F]{32}',
-        'twilio_account_sid' : 'AC[a-zA-Z0-9_\-]{32}',
-        'twilio_app_sid' : 'AP[a-zA-Z0-9_\-]{32}',
-        'paypal_braintree_access_token' : 'access_token\$production\$[0-9a-z]{16}\$[0-9a-f]{32}',
-        'square_oauth_secret' : 'sq0csp-[ 0-9A-Za-z\-_]{43}|sq0[a-z]{3}-[0-9A-Za-z\-_]{22,43}',
-        'square_access_token' : 'sqOatp-[0-9A-Za-z\-_]{22}|EAAA[a-zA-Z0-9]{60}',
-        'stripe_standard_api' : 'sk_live_[0-9a-zA-Z]{24}',
-        'stripe_restricted_api' : 'rk_live_[0-9a-zA-Z]{24}',
-        'github_access_token' : '[a-zA-Z0-9_-]*:[a-zA-Z0-9_\-]+@github\.com*',
-        'rsa_private_key' : '-----BEGIN RSA PRIVATE KEY-----',
-        'ssh_dsa_private_key' : '-----BEGIN DSA PRIVATE KEY-----',
-        'ssh_dc_private_key' : '-----BEGIN EC PRIVATE KEY-----',
-        'pgp_private_block' : '-----BEGIN PGP PRIVATE KEY BLOCK-----',
-        'json_web_token' : 'ey[A-Za-z0-9_-]*\.[A-Za-z0-9._-]*|ey[A-Za-z0-9_\/+-]*\.[A-Za-z0-9._\/+-]*'
+        # add your regex here
+        regexs = {
+            'google_api' : 'AIza[0-9A-Za-z-_]{35}',
+            'docs_file_exetension' : '^.*\\.(xls|xlsx|doc|docx)$',
+            'bitcoin_address' : '([13][a-km-zA-HJ-NP-Z0-9]{26,33})',
+            'slack_api_key' : 'xox.-[0-9]{12}-[0-9]{12}-[0-9a-zA-Z]{24}',
+            'us_cn_zipcode' : '(^\\d{5}(-\\d{4})?$)|(^[ABCEGHJKLMNPRSTVXY]\\d[A-Z] *\\d[A-Z]\\d$)',
+            'google_cloud_platform_auth' : '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}',
+            'google_cloud_platform_api' : '[A-Za-z0-9_]{21}--[A-Za-z0-9_]{8}',
+            'amazon_secret_key' : '[A-Za-z0-9/+=]{40}',
+            'gmail_auth_token' : '[0-9]+-[0-9A-Za-z_]{32}\\.apps\\.googleusercontent\\.com',
+            'github_auth_token' : '[0-9a-fA-F]{40}',
+            'Instagram_token' : '[0-9a-fA-F]{7}\\.[0-9a-fA-F]{32}',
+            'twitter_access_token' : '[1-9][0-9]+-[0-9a-zA-Z]{40}',
+            'firebase' : 'AAAA[A-Za-z0-9_-]{7}:[A-Za-z0-9_-]{140}',
+            'google_captcha' : '(?:6L[0-9A-Za-z-_]{38}|6[0-9a-zA-Z_-]{39})',
+            'google_oauth' : 'ya29\\.[0-9A-Za-z\\-_]+',
+            'amazon_aws_access_key_id' : 'A[SK]IA[0-9A-Z]{16}',
+            'amazon_mws_auth_token' : 'amzn\\.mws\\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}',
+            'amazon_aws_url' : 's3\\.amazonaws\\.com[/]+|[a-zA-Z0-9_-]*\\.s3\\.amazonaws\\.com',
+            'facebook_access_token' : 'EAACEdEose0cBA[0-9A-Za-z]+',
+            'authorization_basic' : 'basic\\s*[a-zA-Z0-9=:_\\+\\/-]+',
+            'authorization_bearer' : 'bearer\\s*[a-zA-Z0-9_\\-\\.=:_\\+\\/]+',
+            'authorization_api' : 'api(?:[\\s:_-]*key)?[\\s:_-]*[a-zA-Z0-9_\\-]+',
+            'mailgun_api_key' : 'key-[0-9a-zA-Z]{32}',
+            'twilio_api_key' : 'SK[0-9a-fA-F]{32}',
+            'twilio_account_sid' : 'AC[a-zA-Z0-9_\\-]{32}',
+            'twilio_app_sid' : 'AP[a-zA-Z0-9_\\-]{32}',
+            'paypal_braintree_access_token' : 'access_token\\$production\\$[0-9a-z]{16}\\$[0-9a-f]{32}',
+            'square_oauth_secret' : 'sq0csp-[0-9A-Za-z\\-_]{43}|sq0[a-z]{3}-[0-9A-Za-z\\-_]{22,43}',
+            'square_access_token' : 'sqOatp-[0-9A-Za-z\\-_]{22}|EAAA[a-zA-Z0-9]{60}',
+            'stripe_standard_api' : 'sk_live_[0-9a-zA-Z]{24}',
+            'stripe_restricted_api' : 'rk_live_[0-9a-zA-Z]{24}',
+            'github_access_token' : '[a-zA-Z0-9_-]+:[a-zA-Z0-9_\\-]+@github\\.com',
+            'rsa_private_key' : '-----BEGIN RSA PRIVATE KEY-----',
+            'ssh_dsa_private_key' : '-----BEGIN DSA PRIVATE KEY-----',
+            'ssh_dc_private_key' : '-----BEGIN EC PRIVATE KEY-----',
+            'pgp_private_block' : '-----BEGIN PGP PRIVATE KEY BLOCK-----',
+            'json_web_token' : 'ey[A-Za-z0-9_-]*\\.[A-Za-z0-9._-]*|ey[A-Za-z0-9_\\/+\\-]*\\.[A-Za-z0-9._\\/+\\-]*'
+            'amazon_aws_access_key_id": r"A[SK]IA[0-9A-Z]{16}'
+            'amazon_mws_auth_token": r"amzn\.mws\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
+            'amazon_aws_url": r"(?:s3\.amazonaws\.com[/]+|[a-zA-Z0-9_-]+\.s3\.amazonaws\.com)'
+            'aws_secret_access_key": r"[A-Za-z0-9/+=]{40}'
+            'aws_session_token": r"[A-Za-z0-9/+=]{20,}'
     }
     regex = r"[:|=|\'|\"|\s*|`|´| |,|?=|\]|\|//|/\*}](%%regex%%)[:|=|\'|\"|\s*|`|´| |,|?=|\]|\}|&|//|\*/]"
     issuename = "SecretFinder: %s"
